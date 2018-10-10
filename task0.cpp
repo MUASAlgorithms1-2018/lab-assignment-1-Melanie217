185d75becedbe241f33d5cf0de321f4fd8de7571
Melanie <muell162@hm.edu>
2018-10-10 19:49:09 +0200

    finished 1 started on first real method

---
+++ task0.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int test() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
---
+++ task1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int cubic_mss(const int pInt[5]) {
+
+    return 0;
+}
+
+
+int main () {
+    static const int arr1[] = {1, 2, -30, 4, 5};
+    int mss1 = cubic_mss(arr1);
+    cout << mss1 << endl;
+
+    return 0;
+}
+
