9b38080a4aefa2e80f40d5a89a3745cd91fd0259
Melanie <muell162@hm.edu>
2018-10-10 20:32:46 +0200

    found way to get array size

--- task1.cpp
+++ task1.cpp
@@ -2,15 +2,15 @@
 
 using namespace std;
 
-int cubic_mss(const int pInt[5]) {
-
-    return 0;
+int cubic_mss(const int list[], int size) {
+    int n = size/sizeof(int);
+    return n;
 }
 
 
 int main () {
     static const int arr1[] = {1, 2, -30, 4, 5};
-    int mss1 = cubic_mss(arr1);
+    int mss1 = cubic_mss(arr1, sizeof(arr1));
     cout << mss1 << endl;
 
     return 0;
