28e78a8983a210ecff8c72d1cee86dace1d87001
Melanie <muell162@hm.edu>
2018-10-11 12:35:59 +0200

    finished cubic mss

--- task1.cpp
+++ task1.cpp
@@ -20,12 +20,46 @@ int cubic_mss(const int list[], int size) {
     return maxSum;
 }
 
+int quad_mss(const int list[], int size) {
+    int n = size/sizeof(int);
+    int maxSum = 0;
+    int thisSum;
+    for (int i = 1; i <= n ; ++i) {
+        thisSum = 0;
+        for (int j = i; j <= n; ++j) {
+            thisSum = thisSum + list[j];
+            if (thisSum > maxSum) {
+                maxSum = thisSum;
+            }
+        }
+    }
+    return maxSum;
+}
+
+int lin_mss(const int list[], int size) {
+    int n = size/sizeof(int);
+    int maxSum = 0;
+    int thisSum = 0;
+    for (int i = 1; i <= n ; ++i) {
+        thisSum = thisSum + list[i];
+        if (thisSum > maxSum) {
+            maxSum = thisSum;
+        } else if (thisSum < 0) {
+            thisSum = 0;
+        }
+    }
+    return maxSum;
+}
+
 
 int main () {
     static const int arr1[] = {1, 2, -30, 4, 5};
-    int mss1 = cubic_mss(arr1, sizeof(arr1));
-    cout << mss1 << endl;
-
+    int cubic = cubic_mss(arr1, sizeof(arr1));
+    cout << "Cubic Solution:  " << cubic << endl;
+    int quad = quad_mss(arr1, sizeof(arr1));
+    cout << "Quadratic Solution:  " << quad << endl;
+    int lin = lin_mss(arr1, sizeof(arr1));
+    cout << "Linear Solution:  " << lin << endl;
     return 0;
 }
 
